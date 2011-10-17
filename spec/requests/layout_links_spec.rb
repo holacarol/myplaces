3d4b887e510ae7caf02b9ce1788c05ea42fddde4
Carolina <carolgarcia@dit.upm.es>
2011-10-17 14:15:19 +0200

    Done with sign in

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -21,5 +21,36 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
+
+  describe "when not signed in" do
+    it "should have a signin link" do
+      visit root_path
+      response.should have_selector("a", :href => new_user_session_path,
+                                         :content => "Sign in")
+    end
+  end
+
+  describe "when signed in" do
+
+    before(:each) do
+      @user = Factory(:user)
+      visit new_user_session_path
+      fill_in :email,    :with => @user.email
+      fill_in :password, :with => @user.password
+      click_button
+    end
+
+    it "should have a signout link" do
+      visit root_path
+      response.should have_selector("a", :href => destroy_user_session_path,
+                                         :content => "Sign out")
+    end
+
+    it "should have a profile link" do
+      visit root_path
+      response.should have_selector("a", :href => user_path(@user),
+                                         :content => "Profile")
+    end
+  end
 end
 
