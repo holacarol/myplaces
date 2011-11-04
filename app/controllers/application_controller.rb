170cd087262894934c9f9e75cf0af61145b2fc2a
Carolina <carolgarcia@dit.upm.es>
2011-11-04 12:02:11 +0100

    Modified after_sign_in_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
     # Overwriting the sign_in redirect path method
   def after_sign_in_path_for(resource_or_scope)
     user_path(resource_or_scope)
+    #root_path
   end
 
 end
